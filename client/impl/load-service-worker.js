b32f033a3bbed940195b27b36fd32329b9fe4b51
beMaddz <madhu05ghosh@gmail.com>
2021-07-19 12:19:10 +0530

    Code clean up

--- client/impl/load-service-worker.js
+++ client/impl/load-service-worker.js
@@ -7,10 +7,19 @@ export function registerServiceWorker({
   mountAt = global.qtMountAt || "",
 }) {
   if (enableServiceWorker && navigator.serviceWorker) {
-    console.log("Registering worker!!!", serviceWorkerLocation);
+    console.log("Registering Service Worker!!!", serviceWorkerLocation);
     return navigator.serviceWorker.register(`${mountAt}${serviceWorkerLocation}`);
   }
   return Promise.resolve(null);
+}
+
+function updateOneSignalWorker(page, opts) {
+  let { config: { "theme-attributes": pageThemeAttributes = {} } = {} } = page;
+  let version = pageThemeAttributes["cache-burst"];
+
+  registerServiceWorker({ ...opts, serviceWorkerLocation: `/OneSignalSDKWorker.js?version=${version}` }).then(() =>
+    console.log("Updated OneSignal Worker")
+  );
 }
 
 export function setupServiceWorkerUpdates(serviceWorkerPromise, app, store, page, opts = {}) {
@@ -22,14 +31,9 @@ export function setupServiceWorkerUpdates(serviceWorkerPromise, app, store, page
     if (registration.update) {
       app.updateServiceWorker = () =>
         registration.update().then(() => store.dispatch({ type: SERVICE_WORKER_UPDATED }));
+
       if (global.OneSignal) {
-        let { config: { "theme-attributes": pageThemeAttributes = {} } = {} } = page;
-        let version = pageThemeAttributes["cache-burst"];
-        app.reRegisterServiceWorker = () =>
-          registerServiceWorker({
-            ...opts,
-            serviceWorkerLocation: `/OneSignalSDKWorker.js?version=${version}`,
-          }).then(() => console.log("Re-registered onesignal worker"));
+        app.updateOneSignalWorker = () => updateOneSignalWorker(page, opts);
       }
     }
 
@@ -40,27 +44,25 @@ export function setupServiceWorkerUpdates(serviceWorkerPromise, app, store, page
 }
 
 function updateServiceWorker(app) {
-  console.log(`updating service worker due to config change`);
-  app.updateServiceWorker && app.updateServiceWorker();
-}
-
-function reRegisterServiceWorker(app) {
-  console.log(`re-register onesignal service worker due to config change`);
-  app.reRegisterServiceWorker && app.reRegisterServiceWorker();
+  if (global.OneSignal) {
+    app.updateOneSignalWorker && app.updateOneSignalWorker();
+  } else {
+    app.updateServiceWorker && app.updateServiceWorker();
+  }
 }
 
 export function checkForServiceWorkerUpdates(app, page = {}) {
+  console.log(global.qtVersion);
   if (page.appVersion && app.getAppVersion && app.getAppVersion() < page.appVersion) {
     console && console.log("Updating the Service Worker");
-    global.OneSignal
-      ? app.reRegisterServiceWorker && app.reRegisterServiceWorker()
-      : app.updateServiceWorker && app.updateServiceWorker();
+    updateServiceWorker(app);
   } else if (global && global.qtVersion) {
     /* Check if the config is updated and update the service worker if true */
     const { qtVersion: { configVersion = 0 } = {} } = global;
     const { config: { "theme-attributes": pageThemeAttributes = {} } = {} } = page;
     if ((pageThemeAttributes["cache-burst"] || 0) > parseInt(configVersion)) {
-      global.OneSignal ? reRegisterServiceWorker(app) : updateServiceWorker(app);
+      console.log(`Updating service worker due to config change`);
+      updateServiceWorker(app);
     }
   }
 
