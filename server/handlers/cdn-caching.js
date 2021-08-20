9f61d690a5b3c6cf43d9c8d8a737fee5ada76b11
Pravin Atigre <atigrepravin@gmail.com>
2021-08-20 14:22:39 +0530

    add cloudflare cache control directive

--- server/handlers/cdn-caching.js
+++ server/handlers/cdn-caching.js
@@ -34,6 +34,7 @@ exports.addCacheHeadersToResult = function addCacheHeadersToResult({
     } else {
       if (networkOnly) {
         res.setHeader("Cache-Control", `public,s-maxage=${sMaxAge}`);
+        res.setHeader("Cloudflare-CDN-Cache-Control", `stale-if-error=14400`);
         cdnProviderVal === "akamai" && res.setHeader("Edge-Control", `public,maxage=${sMaxAge}`);
       } else {
         res.setHeader(
