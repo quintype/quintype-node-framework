c690029f997d35af7a659627623e4359b8a96cda
Harshith Raj <harshithraj@Harshiths-MacBook-Pro-2.local>
2022-04-22 15:38:52 +0530

    wip

--- test/babel.js
+++ test/babel.js
@@ -1,14 +1,7 @@
 const ROOT_PATH = require("path").resolve(__dirname, "..");
 
 require("babel-register")({
-  presets: [
-    [
-      "react",
-      {
-        runtime: "automatic",
-      },
-    ],
-  ],
+  presets: ["react"],
   plugins: ["transform-es2015-modules-commonjs", "transform-object-rest-spread", "quintype-assets"],
   ignore(file) {
     return file.startsWith(ROOT_PATH + "/node_modules");
