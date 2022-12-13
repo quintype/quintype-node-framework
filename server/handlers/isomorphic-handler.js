4e8a3b38b9be62571f96e69e8b976ff610daf068
Jeevan Kishore <kishor080@gmail.com>
2022-12-13 13:13:32 +0530

    feat(amp): Decode embed using utf 8 (#345)
    rel: https://github.com/quintype/quintype-amp/pull/409

--- server/handlers/isomorphic-handler.js
+++ server/handlers/isomorphic-handler.js
@@ -140,7 +140,7 @@ exports.handleIsomorphicShell = async function handleIsomorphicShell(
   ).then((result) => {
     res.status(200);
     res.setHeader("Content-Type", "text/html");
-    res.setHeader("Cache-Control", "public,max-age=900");
+    res.setHeader("Cache-Control", "public,max-age=900,stale-while-revalidate=1500,stale-if-error=28800");
     res.setHeader("Vary", "Accept-Encoding");
 
     if (preloadJs) {
