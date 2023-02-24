53ccc8b4e5d0228e7d42d271c3d1e37808d82e37
Jeevan Kishore <kishor080@gmail.com>
2023-02-24 16:32:32 +0530

    Implement webengage route (#354)

---
+++ server/handlers/webengage-notifications.js
@@ -0,0 +1,30 @@
+const get = require("lodash/get");
+const request = require("request-promise");
+
+exports.triggerWebengageNotifications = async function triggerWebengageNotifications(
+  req,
+  res,
+  next,
+  { apiKey, licenseCode }
+) {
+  const eventName = get(req, ["body", "v1", "event", "name"], "");
+  const url = `https://api.webengage.com/v2/accounts/${licenseCode}/business/save-event`;
+
+  try {
+    await request({
+      uri: url,
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+        "content-type": "application/json",
+      },
+      body: { eventName: eventName, eventData: { ...req.body, user_type: "malibu_user" } },
+      json: true,
+    });
+    res.status(200).send("webengage event triggered successfully");
+    return;
+  } catch (error) {
+    res.status(500).send("webengage event failed");
+    return;
+  }
+};
