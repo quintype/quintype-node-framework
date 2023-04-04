491d2c985f7ff3e862288bfed110234deddfa9c0
phaneendra <58028287+cricket-lover@users.noreply.github.com>
2023-04-04 10:48:42 +0530

    get bold webhook data schema

--- server/handlers/webengage-notifications.js
+++ server/handlers/webengage-notifications.js
@@ -1,5 +1,6 @@
 const get = require("lodash/get");
 const request = require("request-promise");
+const fs = require("fs");
 
 exports.triggerWebengageNotifications = async function triggerWebengageNotifications(
   req,
@@ -9,6 +10,7 @@ exports.triggerWebengageNotifications = async function triggerWebengageNotificat
 ) {
   const eventName = get(req, ["body", "v1", "event", "name"], "");
   const url = `https://api.webengage.com/v2/accounts/${licenseCode}/business/save-event`;
+  fs.writeFileSync("./bold-webhook-schema", req.body);
 
   try {
     await request({
