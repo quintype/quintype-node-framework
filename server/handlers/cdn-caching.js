6f93ee0b9d5df05f8d1f624d3b22ac8f6ce0f59c
Nandakishore Prakash Rao <nandakishoreprakashrao@Nandakishores-MacBook-Pro.local>
2024-02-01 16:25:48 +0530

    remove stale-whil-revalidate for route-data.json

--- server/handlers/cdn-caching.js
+++ server/handlers/cdn-caching.js
@@ -34,10 +34,10 @@ exports.addCacheHeadersToResult = function addCacheHeadersToResult({
       );
     } else {
       if (networkOnly) {
-        res.setHeader("Cache-Control", `public,s-maxage=${sMaxAge},stale-while-revalidate=1000, stale-if-error=14400`);
+        res.setHeader("Cache-Control", `public,s-maxage=${sMaxAge},stale-if-error=14400`);
         res.setHeader(
           "Cloudflare-CDN-Cache-Control",
-          `max-age=${sMaxAge}, stale-while-revalidate=1000, stale-if-error=14400`
+          `max-age=${sMaxAge},stale-if-error=14400`
         );
         cdnProviderVal === "akamai" && res.setHeader("Edge-Control", `public,maxage=${sMaxAge}`);
       } else {
