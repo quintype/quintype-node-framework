3581950182862904a62ebb38f50c7976748ec3b2
phaneendra <58028287+cricket-lover@users.noreply.github.com>
2024-03-18 12:41:34 +0530

    change condition for visual stories

--- server/amp/handlers/story-page.js
+++ server/amp/handlers/story-page.js
@@ -41,10 +41,12 @@ async function ampStoryPageHandler(
 ) {
   try {
     const opts = cloneDeep(rest);
-    const isCorrectAmpPath = req.path.startsWith(`${getAmpPageBasePath(opts, config)}/`);
-    if (!(isCorrectAmpPath || isVisualStory)) {
-      return next();
-    }
+    const isCorrectAmpPath = isVisualStory
+      ? req.path.startsWith(`/ampstories`)
+      : req.path.startsWith(`${getAmpPageBasePath(opts, config)}/`);
+
+    if (!isCorrectAmpPath) return next();
+
     const redirectUrls = opts && opts.redirectUrls;
     const getEnableAmp = get(opts, ["enableAmp"], true);
     const enableAmp = typeof getEnableAmp === "function" ? opts.enableAmp(config) : getEnableAmp;
