f4dcb500ea5f4072e831d989e0cb180de0688cf9
Vishwanath <90732088+vishu2222@users.noreply.github.com>
2024-04-09 15:42:19 +0530

    validate request instead of response

--- server/amp/handlers/infinite-scroll.js
+++ server/amp/handlers/infinite-scroll.js
@@ -6,6 +6,8 @@ const cloneDeep = require("lodash/cloneDeep");
 
 // eslint-disable-next-line consistent-return
 async function storyPageInfiniteScrollHandler(req, res, next, { client, config, ...rest }) {
+  if (!req.query["story-id"]) return res.status(400).send(`Query param "story-id" missing`);
+
   const ampConfig = await config.memoizeAsync("amp-config", async () => await AmpConfig.getAmpConfig(client));
   const opts = cloneDeep(rest);
   const infiniteScrollSource = get(opts, ["featureConfig", "infiniteScroll", "source"], "collection");
@@ -20,19 +22,11 @@ async function storyPageInfiniteScrollHandler(req, res, next, { client, config, 
     infiniteScrollSource,
   });
   const jsonResponse = await infiniteScrollAmp.getResponse();
-  if (jsonResponse instanceof Error) return handleErrorResponse(res, next, jsonResponse);
+  if (jsonResponse instanceof Error) return next(errorMessage);
   res.set("Content-Type", "application/json; charset=utf-8");
   setCorsHeaders({ req, res, next, publisherConfig: config });
 
   if (!res.headersSent) return res.send(jsonResponse);
-}
-
-function handleErrorResponse(res, next, error) {
-  const errorMessage = error.message;
-  if (errorMessage === `Query param "story-id" missing`) {
-    return res.status(400).send(errorMessage);
-  }
-  return next(errorMessage);
 }
 
 module.exports = { storyPageInfiniteScrollHandler };
