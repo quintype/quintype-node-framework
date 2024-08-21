3fe23562a6bf1e02930a84f2008d375370fc5ba1
Reena <reena@quintype.com>
2024-08-21 10:02:10 +0530

    remove optimizer

--- server/amp/helpers/index.js
+++ server/amp/helpers/index.js
@@ -1,9 +1,7 @@
 const { setCorsHeaders } = require("./set-cors-headers");
-const { optimize } = require("./optimize-amp-html");
 const { getDomainSpecificOpts } = require("./get-domain-specific-opts");
 
 module.exports = {
   setCorsHeaders,
-  optimize,
   getDomainSpecificOpts,
 };
