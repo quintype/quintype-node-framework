86dc0acf55e38c5f5754e905218100193c88f678
Reena <reena@quintype.com>
2024-08-22 16:55:18 +0530

    upgrade amp optimizer

--- server/amp/helpers/optimize-amp-html.js
+++ server/amp/helpers/optimize-amp-html.js
@@ -1,15 +1,11 @@
-const AmpOptimizer = require("@ampproject/toolbox-optimizer");
+const AmpOptimizer = require('@ampproject/toolbox-optimizer')
 
-const ampOptimizer = AmpOptimizer.create({
-  autoAddMandatoryTags: false,
-  autoExtensionImport: false,
-  preloadHeroImage: false,
-});
+const ampOptimizer = AmpOptimizer.create()
 
-async function optimize(ampHtml) {
-  const optimizedAmp = ampOptimizer.transformHtml(ampHtml);
+async function optimize (ampHtml) {
+  const optimizedAmp = ampOptimizer.transformHtml(ampHtml)
 
-  return optimizedAmp;
+  return optimizedAmp
 }
 
-module.exports = { optimize };
+module.exports = { optimize }
