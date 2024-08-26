2a36a6bb8d396a375517e7c0af4e40a4bb5477c9
Reena <reena@quintype.com>
2024-08-26 08:38:37 +0530

    wip

--- server/amp/helpers/optimize-amp-html.js
+++ server/amp/helpers/optimize-amp-html.js
@@ -1,11 +1,5 @@
 const AmpOptimizer = require('@ampproject/toolbox-optimizer')
 
-// const ampOptimizer = AmpOptimizer.create({
-//   autoAddMandatoryTags: false,
-//   autoExtensionImport: false,
-//   preloadHeroImage: false
-// })
-
 const ampOptimizer = AmpOptimizer.create()
 
 async function optimize (ampHtml) {
