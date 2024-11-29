732b027b243c0823073a11d7e4370e32c4715cfc
Reena <reena@quintype.com>
2024-11-29 15:11:05 +0530

    add logs

--- server/data-loader-helpers.js
+++ server/data-loader-helpers.js
@@ -1,20 +1,20 @@
 // istanbul ignore file
 // This file is not documented
 
-const { Story, Collection } = require("./api-client");
-const templateOptions = require("./impl/template-options");
+const { Story, Collection } = require('./api-client')
+const templateOptions = require('./impl/template-options')
 
-exports.catalogDataLoader = function catalogDataLoader(client, config) {
-  return Story.getStories(client).then((stories) => {
+exports.catalogDataLoader = function catalogDataLoader (client, config) {
+  return Story.getStories(client).then(stories => {
     return {
-      stories: stories.map((story) => story.asJson()),
+      stories: stories.map(story => story.asJson()),
       cacheKeys: [`static`],
-      templateOptions,
-    };
-  });
-};
+      templateOptions
+    }
+  })
+}
 
-exports.homeCollectionOrStories = function homeCollectionOrStories(
+exports.homeCollectionOrStories = function homeCollectionOrStories (
   client,
   depth = 1,
   getStoryLimits,
@@ -23,12 +23,12 @@ exports.homeCollectionOrStories = function homeCollectionOrStories(
   customLayouts = [],
   defaultNestedLimit = null
 ) {
-  const {qtInternalAppsKey = "", previewId = "", ...rest} = params || {};
-
+  const { qtInternalAppsKey = '', previewId = '', ...rest } = params || {}
+  console.log('homeCollectionOrStories------', params)
   return Collection.getCollectionBySlug(
     client,
-    "home",
-    { "item-type": "collection", ...rest },
+    'home',
+    { 'item-type': 'collection', ...rest },
     {
       depth,
       ...(getStoryLimits && { storyLimits: getStoryLimits() }),
@@ -38,25 +38,26 @@ exports.homeCollectionOrStories = function homeCollectionOrStories(
       qtInternalAppsKey,
       previewId
     }
-  ).then((collection) => {
-    if (collection) return collection;
-    return Story.getStories(client).then((stories) =>
+  ).then(collection => {
+    console.log('collection homeCollectionOrStories---', collection)
+    if (collection) return collection
+    return Story.getStories(client).then(stories =>
       Collection.build({
-        slug: "home",
-        name: "Home",
-        template: "default",
+        slug: 'home',
+        name: 'Home',
+        template: 'default',
         items: [
           {
-            type: "collection",
-            name: "Home",
-            template: "default",
-            items: stories.map((story) => ({
-              type: "story",
-              story: story.asJson(),
-            })),
-          },
-        ],
+            type: 'collection',
+            name: 'Home',
+            template: 'default',
+            items: stories.map(story => ({
+              type: 'story',
+              story: story.asJson()
+            }))
+          }
+        ]
       })
-    );
-  });
-};
+    )
+  })
+}
