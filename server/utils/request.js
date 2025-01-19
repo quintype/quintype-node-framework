25f0e08004dd7e3c0dce375020e731a69f0b3418
U Sai Rajesh <srajesh636@gmail.com>
2025-01-19 18:57:24 +0530

    fix typo

--- server/utils/request.js
+++ server/utils/request.js
@@ -2,7 +2,9 @@ import { v4 as uuidv4 } from 'uuid';
 
 export const logRequest = (req, res) => {
   const logger = require("../logger");
-  const getClient = require('./api-client').getClient;
+  const getClient = require('../api-client').getClient;
+
+
 
   const qtTraceId = (req && req.headers && req.headers['qt-trace-id']) || uuidv4();
   const { path } = req;
