9b55504c28b42bf89fa3698c6f674b396f4e2a0b
U Sai Rajesh <srajesh636@gmail.com>
2025-01-19 18:52:52 +0530

    refactor

--- server/utils/request.js
+++ server/utils/request.js
@@ -1,9 +1,9 @@
-import { getClient } from '../api-client';
-const { v4: uuidv4 } = require('uuid');
-
+import { v4 as uuidv4 } from 'uuid';
 
 export const logRequest = (req, res) => {
   const logger = require("../logger");
+  const getClient = require('./api-client').getClient;
+
   const qtTraceId = (req && req.headers && req.headers['qt-trace-id']) || uuidv4();
   const { path } = req;
   const { statusCode, method, statusMessage, headers } = res;
