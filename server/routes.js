1eacdab6b4709ea22792094f1f3969ef5e772d16
U Sai Rajesh <srajesh636@gmail.com>
2025-01-19 18:04:03 +0530

    fix typo

--- server/routes.js
+++ server/routes.js
@@ -104,8 +104,8 @@ exports.upstreamQuintypeRoutes = function upstreamQuintypeRoutes(
     // Attach QT-TRACE-ID to all the request going to sketches.
     const logger = require("./logger");
     const qtTraceId = (req && req.headers && req.headers['qt-trace-id']) || uuidv4();
-    const { path } = req;
-    const { statusCode, method, statusMessage } = res;
+    const { path, } = req;
+    const { statusCode, method, statusMessage, headers } = res;
     req.headers['qt-trace-id'] = qtTraceId;
 
     const loggedDataAttributes = {
