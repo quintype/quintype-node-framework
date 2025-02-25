ade87b29fb9a4fc1671bf04b20fc5ce6d81375d3
Reena <reena@quintype.com>
2025-02-25 13:46:32 +0530

    use firebase admin

--- server/handlers/fcm-registration-handler.js
+++ server/handlers/fcm-registration-handler.js
@@ -1,38 +1,51 @@
-const { get } = require("lodash");
-const request = require("request-promise");
+const { get } = require('lodash')
+const request = require('request-promise')
+const admin = require('firebase-admin')
+const serviceAccount = require('https://github.com/ReenaSingh07/fcm-service-account/blob/main/pbahead.json')
 
-exports.registerFCMTopic = async function registerFCM(
+exports.registerFCMTopic = async function registerFCM (
   req,
   res,
   next,
   { config, client, publisherConfig, fcmServerKey }
 ) {
-  const token = get(req, ["body", "token"], null);
+  const token = get(req, ['body', 'token'], null)
   if (!token) {
-    res.status(400).send("No Token Found");
-    return;
+    res.status(400).send('No Token Found')
+    return
   }
 
-  const serverKey = typeof fcmServerKey === "function" ? await fcmServerKey(config) : fcmServerKey;
-
-  if (!serverKey) {
-    res.status(500).send("Server Key is not available");
-    return;
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  })
+  console.log('admin------', admin)
+  async function generateAccessToken () {
+    const token = await admin.credential.cert(serviceAccount).getAccessToken()
+    console.log('OAuth2 Access Token:', token)
+    return token
   }
-  const url = `https://iid.googleapis.com/iid/v1/${token}/rel/topics/all`;
+
+  const oauthToken = generateAccessToken()
+
+  if (!oauthToken) {
+    res.status(500).send('Oauth Token is not available')
+    return
+  }
+
+  const url = `https://iid.googleapis.com/iid/v1/${token}/rel/topics/all`
   try {
     await request({
       uri: url,
-      method: "POST",
+      method: 'POST',
       headers: {
-        Authorization: `key=${serverKey}`,
-        "content-type": "application/json",
-      },
-    });
-    res.status(200).send("Registration Done Suceessfuly");
-    return;
+        Authorization: `Bearer ${oauthToken}`,
+        'content-type': 'application/json'
+      }
+    })
+    res.status(200).send('Registration Done Suceessfuly')
+    return
   } catch (error) {
-    res.status(500).send("FCM Subscription Failed");
-    return;
+    res.status(500).send('FCM Subscription Failed', error)
+    return
   }
-};
+}
