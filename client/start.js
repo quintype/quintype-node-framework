73c55cd29c0d54eae2432cd047234d7b57f9b2e3
Reena <reena@quintype.com>
2025-03-04 08:50:48 +0530

    comment initialize fcm

--- client/start.js
+++ client/start.js
@@ -301,16 +301,16 @@ export function startApp(renderApplication, reducers, opts) {
 
     store.dispatch({ type: NAVIGATE_TO_PAGE, page, currentPath: path });
 
-    if (opts.firebaseConfig) {
-      const fcm = typeof opts.firebaseConfig === "function" ? opts.firebaseConfig(page) : opts.firebaseConfig;
-      if (fcm) {
-        const mssgSenderId = fcm.messagingSenderId;
-        const projectId = fcm.projectId;
-        const apiKey = fcm.apiKey;
-        console.log("fcm start app")
-        if (mssgSenderId && projectId && apiKey) initializeFCM(fcm);
-      }
-    }
+    // if (opts.firebaseConfig) {
+    //   const fcm = typeof opts.firebaseConfig === "function" ? opts.firebaseConfig(page) : opts.firebaseConfig;
+    //   if (fcm) {
+    //     const mssgSenderId = fcm.messagingSenderId;
+    //     const projectId = fcm.projectId;
+    //     const apiKey = fcm.apiKey;
+    //     console.log("fcm start app")
+    //     if (mssgSenderId && projectId && apiKey) initializeFCM(fcm);
+    //   }
+    // }
 
     const { config: { "theme-attributes": pageThemeAttributes = {} } = {} } = page;
     const version = pageThemeAttributes["cache-burst"] || app.getAppVersion();
